58a5926b85f6e58b9b16dcce1dcef13433d62de0
Vincent Rattanopas <vin_ian@hotmail.com>
2026-05-02 00:24:24 +0700

    fix: removed git markers from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This new section tells Vite to ignore the problematic library
-  // during its optimization step, which fixes the error.
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 })
->>>>>>> master
